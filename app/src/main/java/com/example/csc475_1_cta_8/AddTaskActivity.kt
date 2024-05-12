b67bbe8b64ef8053be34d8534864ceb6d1722f27
hkrebs55 <hrkrebs55@gmail.com>
2024-05-12 22:26:33 +0200

    Push 2

--- app/src/main/java/com/example/csc475_1_cta_8/AddTaskActivity.kt
+++ app/src/main/java/com/example/csc475_1_cta_8/AddTaskActivity.kt
@@ -1,4 +1,80 @@
 package com.example.csc475_1_cta_8
 
-class AddTaskActivity {
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.gson.Gson
+import com.google.gson.JsonArray;
+import java.util.UUID
+
+class AddTaskActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+    private var gson: Gson = Gson()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+        sharedPreferences = getSharedPreferences("TaskPrefs", Context.MODE_PRIVATE)
+        var selectedDate = ""
+        var taskName = ""
+        var taskDescription = ""
+
+        findViewById<CalendarView>(R.id.calendarView).setOnDateChangeListener { _, year, month, dayOfMonth ->
+            selectedDate = "$dayOfMonth/${month + 1}/$year"
+        }
+
+
+        findViewById<Button>(R.id.backButton).setOnClickListener {
+            finish()
+        }
+
+        findViewById<TextInputEditText>(R.id.taskNameInput).addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                taskName = s.toString()
+            }
+        })
+
+        findViewById<TextInputEditText>(R.id.taskDescriptionInput).addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                taskDescription = s.toString()
+            }
+        })
+
+        findViewById<Button>(R.id.saveButton).setOnClickListener {
+            if (taskName != "" && taskDescription != "" && selectedDate != "") {
+                saveTask(selectedDate, taskName, taskDescription)
+                setResult(Activity.RESULT_OK)
+                finish()
+            }
+        }
+    }
+
+    private fun saveTask(date: String, taskName: String, taskDescription: String) {
+        val editor = sharedPreferences.edit()
+        val newTask = gson.toJson(Task(date, taskName, taskDescription, false, UUID.randomUUID()))
+        val existingTasks = sharedPreferences.getString(date, null)
+        val taskList: JsonArray?
+
+        if (existingTasks == null) {
+            taskList = JsonArray();
+            taskList.add(newTask)
+        } else {
+            taskList = gson.fromJson(existingTasks, JsonArray::class.java);
+            taskList.add(newTask)
+        }
+
+        editor.putString(date, gson.toJson(taskList))
+        editor.apply()
+    }
 }
--- app/src/main/java/com/example/csc475_1_cta_8/MainActivity.kt
+++ app/src/main/java/com/example/csc475_1_cta_8/MainActivity.kt
@@ -2,79 +2,66 @@ package com.example.csc475_1_cta_8
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.Button
 import android.widget.CalendarView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.textfield.TextInputEditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
-import com.google.gson.JsonArray;
-import java.util.UUID
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
-class AddTaskActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
     private var gson: Gson = Gson()
+    private var selectedDate = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
         sharedPreferences = getSharedPreferences("TaskPrefs", Context.MODE_PRIVATE)
-        var selectedDate = ""
-        var taskName = ""
-        var taskDescription = ""
+        print(sharedPreferences.all.values)
+        setContentView(R.layout.activity_main)
 
-        findViewById<CalendarView>(R.id.calendarView).setOnDateChangeListener { _, year, month, dayOfMonth ->
+        val calendarView: CalendarView = findViewById(R.id.calendarView)
+        val addTaskButton: Button = findViewById(R.id.addTaskButton)
+        initializeTaskList()
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             selectedDate = "$dayOfMonth/${month + 1}/$year"
+            initializeTaskList()
         }
 
-
-        findViewById<Button>(R.id.backButton).setOnClickListener {
-            finish()
-        }
-
-        findViewById<TextInputEditText>(R.id.taskNameInput).addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                taskName = s.toString()
-            }
-        })
-
-        findViewById<TextInputEditText>(R.id.taskDescriptionInput).addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                taskDescription = s.toString()
-            }
-        })
-
-        findViewById<Button>(R.id.saveButton).setOnClickListener {
-            if (taskName != "" && taskDescription != "" && selectedDate != "") {
-                saveTask(selectedDate, taskName, taskDescription)
-                setResult(Activity.RESULT_OK)
-                finish()
-            }
+        addTaskButton.setOnClickListener {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            resultLauncher.launch(intent)
         }
     }
 
-    private fun saveTask(date: String, taskName: String, taskDescription: String) {
-        val editor = sharedPreferences.edit()
-        val newTask = gson.toJson(Task(date, taskName, taskDescription, false, UUID.randomUUID()))
-        val existingTasks = sharedPreferences.getString(date, null)
-        val taskList: JsonArray?
+    private fun initializeTaskList() {
+        val taskRecyclerView: RecyclerView = findViewById(R.id.taskList)
 
-        if (existingTasks == null) {
-            taskList = JsonArray();
-            taskList.add(newTask)
-        } else {
-            taskList = gson.fromJson(existingTasks, JsonArray::class.java);
-            taskList.add(newTask)
+        if (sharedPreferences.all.isNotEmpty() && selectedDate != "") {
+            taskRecyclerView.layoutManager = LinearLayoutManager(this)
+            val listType: Type = object : TypeToken<List<Task>>() {}.type
+            val tasksString = sharedPreferences.getString(selectedDate, "").toString().replace("[\"", "[").replace("\"]", "]").replace("\\", "").replace("}\",\"{", "},{")
+            if (tasksString != "") {
+                val taskList: List<Task> = gson.fromJson(tasksString, listType)
+                taskRecyclerView.adapter = TaskListAdaptor(this, taskList)
+            } else {
+                val taskList: List<Task> = gson.fromJson("[]", listType)
+                taskRecyclerView.adapter = TaskListAdaptor(this, taskList)
+            }
+
         }
+    }
 
-        editor.putString(date, gson.toJson(taskList))
-        editor.apply()
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            initializeTaskList()
+        }
     }
 }
